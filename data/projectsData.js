2e59ba55d59e85135cd97823b96a0d3ea62eff66
Yasser Tahiri <yasserth19@gmail.com>
2021-07-10 01:19:04 +0100

    Add the Project Requirements Field :rocket:

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,28 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'DogeAPI',
+    description: `an API with high performance built with FastAPI & SQLAlchemy, help to improve connection with your Backend Side.`,
+    imgSrc: '/static/images/project/01.png',
+    href: 'https://github.com/yezz123/DogeAPI',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Athena',
+    description: `Behind lines , Why I choose this name cause it look more dramatic for the project.I am inspired a lot from Athena for example, how she was a great woman with a great power & how she inspire from life and acting for civilization.
+    That is for you, cause when you try to escape and use Athena, you break into a world of 0’s & 1’s for testing your power of coding and breaking into the hidden part.`,
+    imgSrc: '/static/images/project/02.png',
+    href: 'https://github.com/yezz123/Athena',
+  },
+  {
+    title: 'My Business',
+    description: `Software application for businesses Application software focused on businesses is implementations IT departments use to enhance business productivity, searching for superior performance and devoted to create value and therefore competitive advantage.`,
+    imgSrc: '/static/images/project/04.gif',
+    href: 'https://github.com/GDGSNF/My-Business',
+  },
+  {
+    title: 'PXXTF',
+    description: ` A framework that Explores kernel vulnerabilities, network vulnerabilities. Is a powerful framework includes a lot of tools for beginners.`,
+    imgSrc: '/static/images/project/03.jpg',
+    href: 'https://github.com/GDGSNF/PXXTF',
   },
 ]
 
