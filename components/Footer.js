1ff2b2161d397abcac6df1a0ead91a1adf272225
Yasser Tahiri <yasserth19@gmail.com>
2021-07-14 05:30:36 +0100

    Fix : Delete Icons Footers :rocket:

--- components/Footer.js
+++ components/Footer.js
@@ -6,31 +6,23 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://yezz.me/">
             <div>{siteMetadata.author}</div>
           </Link>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            <div>Theme by timlrx</div>
+          </Link>
           <div>{` • `}</div>
           <Link href="https://vercel.com/">
             <div> Deployed on Vercel</div>
           </Link>
-          <div>{` • `}</div>
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            <div>Theme by timlrx</div>
-          </Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/yezz123/Blog/blob/master/LICENSE">MIT License</Link>
+        <div className="flex mb-8 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/yezz123/Blog/">Open Source </Link>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
       </div>
     </footer>
