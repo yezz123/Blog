e049fdf4a5e1b60de469585b5b4a07700b3c1981
Yasser Tahiri <yasserth19@gmail.com>
2021-07-14 05:05:10 +0100

    Revert "Fix : Footer Icons ungenerated :rocket:"
    This reverts commit 4c2b2d7726cacff2fdc9a326bf5a296c6925b40d.

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,32 @@ import Head from 'next/head'
 import { SEO } from '@/components/SEO'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
+
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
