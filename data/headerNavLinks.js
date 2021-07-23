ae9cc6e21221be108119174f6a8f75e176f3e2c6
Yasser Tahiri <yasserth19@gmail.com>
2021-07-23 13:26:04 +0100

    🎨 Change Project Tag to link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://github.com/yezz123', title: 'Projects' },
   { href: 'https://yezz.me', title: 'Home' },
 ]
 
