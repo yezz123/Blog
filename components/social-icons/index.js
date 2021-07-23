8f6c4ef5e7b39cee92e6f75dd6f957a87f8ee382
Yasser Tahiri <yasserth19@gmail.com>
2021-07-23 13:25:19 +0100

    🛠 Fix Components Content `icons`

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -1,6 +1,5 @@
 import Mail from './mail.svg'
 import Github from './github.svg'
-import Facebook from './facebook.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
 
@@ -9,7 +8,6 @@ import Twitter from './twitter.svg'
 const components = {
   mail: Mail,
   github: Github,
-  facebook: Facebook,
   linkedin: Linkedin,
   twitter: Twitter,
 }
