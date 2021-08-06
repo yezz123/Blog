c2fac3c54fc00aa45bd349de7c9f2b16da658600
Yasser Tahiri <yasserth19@gmail.com>
2021-08-06 14:40:12 +0100

    ⚙ Delete About me Tag

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-  { href: 'https://github.com/yezz123', title: 'Projects' },
   { href: 'https://yezz.me', title: 'Home' },
 ]
 
